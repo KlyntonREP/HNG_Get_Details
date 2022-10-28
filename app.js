2ebcc3c4cc2a579dcb35c9823ea26f28e7bda5c1
KlyntonREP <clinkesh57@gmail.com>
2022-10-28 08:22:56 +0100

    Added Cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require ('express');
+const cors = require('cors')
 const fs = require('fs');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get('/api/v1/details', function(req, res){
